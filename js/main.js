6e80fae43241e67cd5b0de5d1e58b51593967a71
Elvis Duru <duruelvis95@gmail.com>
2019-02-10 16:36:19 +0100

    finshed hero section

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Typed Setup
+
+var options = {
+    strings: [
+        `We offer creative working environments with unique entrepreneurial spirit.^1000\n`,
+        `We provide tenant experience for agile workspaces.^1000\n`,
+        `Meet.^500\n Collaborate.^500\n Grow.`,
+    ],
+    typeSpeed: 40,
+    backSpeed: 0,
+    smartBackspace: true,
+    loop: true
+}
+
+var typed = new Typed('#hero-text', options)
