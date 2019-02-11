7826bda7eee120cdb131d62795f8a2bd4e75fd6c
Elvis Duru <duruelvis95@gmail.com>
2019-02-11 06:21:48 +0100

    finshed landing page

--- js/main.js
+++ js/main.js
@@ -12,4 +12,18 @@ var options = {
     loop: true
 }
 
-var typed = new Typed('#hero-text', options)
+var typed = new Typed('#hero-text', options);
+
+// NAV
+
+var openNavBtn = document.querySelector('.open-nav');
+var closeNavBtn = document.querySelector('.close-nav');
+var mobileNav = document.querySelector('.right-nav');
+
+openNavBtn.addEventListener("click", function() {
+    mobileNav.classList.add('slide-in')
+})
+
+closeNavBtn.addEventListener("click", function() {
+    mobileNav.classList.remove("slide-in");
+})
